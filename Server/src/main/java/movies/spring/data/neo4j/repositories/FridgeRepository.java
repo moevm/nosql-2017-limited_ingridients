7b0cf65573dbbb45402d654bb9a99e9bb753b3c5
JuliyaM <uliya.marushak@gmail.com>
2017-11-23 12:56:36 +0300

    Добавлен репозиторий для работы с холодильниками в БД

---
+++ Server/src/main/java/movies/spring/data/neo4j/repositories/FridgeRepository.java
@@ -0,0 +1,18 @@
+package movies.spring.data.neo4j.repositories;
+
+import movies.spring.data.neo4j.domain.Fridge;
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "fridges", path = "fridges")
+public interface FridgeRepository extends PagingAndSortingRepository<Fridge, Long> {
+
+    @Query("CREATE (userFirdg:Fridge {userID:{userID}})")
+    void createNewFridge(@Param("userID") String userID);
+
+
+    @Query("MATCH (fr:Fridge {userID: {userID}}) RETURN fr")
+    Fridge requestFridge(@Param("userID") String userID);
+}
